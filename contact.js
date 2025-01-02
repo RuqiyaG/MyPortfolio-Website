9e24dcc10b0f4919f51b4191d99d78a3def3cdf5
RuqiyaG <ruqiyageelle@gmail.com>
2025-01-02 14:17:34 +0000

    Update contact.js
    added interactivity for menu and alerts if the form isn't completed correctly.

--- contact.js
+++ contact.js
@@ -1,1 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menuToggle');
+    const menu = document.getElementById('menu');
 
+    menuToggle.addEventListener('click', function() {
+        menu.classList.toggle('active');
+    });
+});
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const responseMessage = document.getElementById('responseMessage');
+
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (emailRegex.test(email)) {
+        responseMessage.textContent = 'Thank you for contacting!';
+        responseMessage.classList.remove('error');
+        responseMessage.classList.add('success');
+
+    } else {
+        responseMessage.textContent = 'Enter an email address.';
+        responseMessage.classList.remove('success');
+        responseMessage.classList.add('error');
+    }
+    document.getElementById('contactForm').reset();
+});
